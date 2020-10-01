5c3739b6eb811b0d4758e2a404a690c84579704b
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-09-30 21:02:28 -0500

    contact form done

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,12 +3,12 @@ import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery({ currentCategory }) {
-  const { name, description } = currentCategory;
+  const { name } = currentCategory;
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
-      <p>{description}</p>
-      <PhotoList  />
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
